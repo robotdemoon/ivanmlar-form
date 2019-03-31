98fc273db199a4be13f2e8466b9ddab193364e08
ivanmlar <ivanmlar@outlook.com>
2019-03-31 14:42:00 -0600

    Primer Commit

---
+++ frontend/app.js
@@ -0,0 +1,32 @@
+import './styles/app.css';
+
+import UI from './UI';
+
+document.getElementById('book-form')
+    .addEventListener('submit', e => {
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const isbn = document.getElementById('isbn').value;
+        const image = document.getElementById('image').files;
+        
+        const formData = new FormData();
+        formData.append('image', image[0])
+        formData.append('title', title);
+        formData.append('author', author);
+        formData.append('isbn', isbn);
+
+        const ui = new UI();
+        ui.addANewBook(formData);
+        ui.renderMessage('New Book add', 'success', 3000);
+        e.preventDefault();
+    });  
+
+document.getElementById('books-cards')
+    .addEventListener('click', e  => {
+        if(e.target.classList.contains('delete')){
+            const ui = new UI();
+            ui.deleteBook(e.target.getAttribute('_id'));
+            ui.renderMessage('Book deleted', 'danger', 3000);
+        }
+        e.preventDefault();
+    });
---
+++ frontend/services/BookService.js
@@ -0,0 +1,34 @@
+class BookService {
+
+    constructor(){
+        this.URI = '/api/books';
+    }
+
+    async getBooks(){
+        const response = await fetch(this.URI);
+        const books = await response.json();
+        return books;
+    }
+
+    async postBook(book){
+        const res =  await fetch(this.URI, {
+            method: 'POST',
+            body: book
+        });
+        const data = await res.json();
+        console.log(data);
+    }
+
+    async deleteBook(bookId){
+        const res = await fetch(`${this.URI}/${bookId}`, {
+            headers:{
+                'Content-type': 'application/json'
+            },
+            method: 'DELETE'
+        });
+        const data = await res.json()
+        console.log(data);
+    }
+}
+
+export default BookService;
